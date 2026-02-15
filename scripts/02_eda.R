2fc8a3c95b31439ca9098b19edd1627c65d611a1
Hamza Latif <lhamza1020@gmail.com>
2026-02-15 06:48:40 +0000

    add script for univariate analysis and respective plots images

---
+++ scripts/02_eda.R
@@ -0,0 +1,40 @@
+# UNIVARIATE ANALYSIS
+
+# Target variable
+ggplot(train) + geom_histogram(aes(train$Item_Outlet_Sales), binwidth = 100, fill = 'darkgreen') + xlab("Item_Outlet_Sales")
+
+# Independent variables (numerical variables)
+p1 = ggplot(combi) + geom_histogram(aes(Item_Weight), binwidth = 0.5, fill = 'blue')
+p2 = ggplot(combi) + geom_histogram(aes(Item_Visibility), binwidth = 0.005, fill = 'blue')
+p3 = ggplot(combi) + geom_histogram(aes(Item_MRP), binwidth = 1, fill = 'blue')
+
+plot_grid(p1, p2, p3, nrow = 1)
+
+# Independent variables (categorical variables)
+ggplot(combi %>% group_by(Item_Fat_Content) %>% summarise(Count = n())) + geom_bar(aes(Item_Fat_Content, Count), stat = 'identity', fill = 'coral1')
+
+#combine 'lf', 'low fat', 'Low Fat' categories and 'reg', 'Regular' categories
+combi$Item_Fat_Content[combi$Item_Fat_Content == 'LF'] = 'Low Fat'
+combi$Item_Fat_Content[combi$Item_Fat_Content == 'low fat'] = 'Low Fat'
+combi$Item_Fat_Content[combi$Item_Fat_Content == 'reg'] = 'Regular'
+
+ggplot(combi %>% group_by(Item_Fat_Content) %>% summarise(Count = n())) + geom_bar(aes(Item_Fat_Content, Count), stat = 'identity', fill = 'coral1')
+
+# other categorical variables
+# plot for Item_Type
+p4 = ggplot(combi %>% group_by(Item_Type) %>% summarise(Count = n())) + geom_bar(aes(Item_Type, Count), stat = 'identity', fill = 'coral1') + xlab('') + geom_label(aes(Item_Type, Count, label = Count), vjust = 0.5) + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + ggtitle('ItemType')
+# plot for Outlet_Identifier
+p5 = ggplot(combi %>% group_by(Outlet_Identifier) %>% summarise(Count = n())) + geom_bar(aes(Outlet_Identifier, Count), stat = 'identity', fill = 'coral1') + geom_label(aes(Outlet_Identifier, Count, label = Count), vjust = 0.5) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+# plot for Outlet_Size
+p6 = ggplot(combi %>% group_by(Outlet_Size) %>% summarise(Count = n())) + geom_bar(aes(Outlet_Size, Count), stat = 'identity', fill = 'coral1') + geom_label(aes(Outlet_Size, Count, label = Count), vjust = 0.5) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+second_row = plot_grid(p5, p6, nrow = 1)
+plot_grid(p4, second_row, ncol = 1)
+
+# rest of categorical variables
+# plot for Outlet_Establishment_Year
+p7 = ggplot(combi %>% group_by(Outlet_Establishment_Year) %>% summarise(Count = n())) + geom_bar(aes(factor(Outlet_Establishment_Year), Count), stat = 'identity', fill = 'coral1') + geom_label(aes(factor(Outlet_Establishment_Year), Count, label = Count), vjust = 0.5) + xlab('Outlet_Establishment_Year') + theme(axis.text.x = element_text(size = 8.5))
+#plot for Outlet_Type
+p8 = ggplot(combi %>% group_by(Outlet_Type) %>% summarise(Count = n())) + geom_bar(aes(Outlet_Type, Count), stat = 'identity', fill = 'coral1') + geom_label(aes(factor(Outlet_Type), Count, label = Count), vjust = 0.5) + theme(axis.text.x = element_text(size = 8.5))
+
+plot_grid(p7, p8, ncol = 2)
