e7a26a47dfeb06b362472d33b68468c03fd095ef
Hamza Latif <lhamza1020@gmail.com>
2026-02-15 05:17:14 +0000

    add script for loading and cleaning datasets

---
+++ scripts/01_data_loading_cleaning.R
@@ -0,0 +1,28 @@
+library(data.table) #used for reading and manipulation of data
+library(dplyr) #used for data manipulation and joining
+library(ggplot2) #used doe plotting
+library(caret) #used for modeling
+library(corrplot) #used for making correlation plot
+library(xgboost) #used for building XGBoost model
+library(cowplot) #used for combining multiple plots
+
+#load datasets
+train = fread("datasets/train_v9rqX0R.csv")
+test = fread("datasets/test_AbJTz2l.csv")
+submission = fread("datasets/sample_submission_8RXa3c6.csv")
+
+# check dimensions of datasets (columns and rows)
+dim(train);dim(test);dim(submission);
+
+# quick glance over the feature names of train and test datasets
+names(train)
+names(test)
+
+# use .str() function to get a short summary of all the features present in the dataset.
+str(train)
+str(test)
+
+# combine train and test data for easier data manipulation and handling
+test[,Item_Outlet_Sales := NA]
+combi = rbind(train, test) #combining train and test datasets
+dim(combi)
