6e21312f2f9f4b8243a7a83b5b1cdb4794f44cb7
Hamza Latif <lhamza1020@gmail.com>
2026-02-16 02:06:36 +0000

    add missing values treatment and respective plots images

--- scripts/02_eda.R
+++ scripts/02_eda.R
@@ -76,3 +76,27 @@ ggplot(train) + geom_violin(aes(Outlet_Size, Item_Outlet_Sales), fill = 'magenta
 p15 = ggplot(train) + geom_violin(aes(Outlet_Location_Type, Item_Outlet_Sales), fill = 'magenta')
 p16 = ggplot(train) + geom_violin(aes(Outlet_Type, Item_Outlet_Sales), fill = 'magenta')
 plot_grid(p15, p16, nrow =2 )
+
+# MISSING VALUES TREATMENT
+# How to find missing values in a variable
+sum(is.na(combi$Item_Weight)) # result is 2439
+
+# Imputing Missing Values
+missing_index = which(is.na(combi$Item_Weight))
+for (i in missing_index){
+  item = combi$Item_Identifier[i]
+  combi$Item_Weight[i] = mean(combi$Item_Weight[combi$Item_Identifier == item], na.rm = T)
+}
+
+sum(is.na(combi$Item_Weight)) # result is 0
+
+# Replacing 0s in Item_Visibility variable
+ggplot(combi) + geom_histogram(aes(Item_Visibility), bins = 100)
+
+zero_index = which(combi$Item_Visibility == 0)
+for (i in zero_index){
+  item = combi$Item_Identifier[i]
+  combi$Item_Visibility[i] = mean(combi$Item_Visibility[combi$Item_Identifier == item], na.rm = T)
+}
+
+ggplot(combi) + geom_histogram(aes(Item_Visibility), bins = 100)
