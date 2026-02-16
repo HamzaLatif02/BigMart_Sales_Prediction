803d50c3ecfda0861e773b8796fe0bb6774c9aca
Hamza Latif <lhamza1020@gmail.com>
2026-02-16 03:35:26 +0000

    added data preprocessing and respective plots images

--- scripts/02_eda.R
+++ scripts/02_eda.R
@@ -136,4 +136,26 @@ ohe = dummyVars("~.", data = combi[,-c('Item_Identifier', 'Outlet_Establishment_
 ohe_df = data.table(predict(ohe, combi[,-c('Item_Identifier', 'Outlet_Establishment_Year', 'Item_Type')]))
 combi = cbind(combi[,'Item_Identifier'], ohe_df)
 
+# DATA PREPROCESSING
+# Removing skewness
+combi[,Item_Visibility := log(Item_Visibility + 1)] # log + 1 to avoid division by zero
+combi[,Price_per_unit_wt := log(Price_per_unit_wt + 1)]
 
+# Scaling numeric predictors
+num_vars = which(sapply(combi, is.numeric)) # index of numeric features
+num_vars_names = names(num_vars)
+combi_numeric = combi[,setdiff(num_vars_names, 'Item_Outlet_Sales'), with = F]
+prep_num = preProcess(combi_numeric, method=c('center', 'scale'))
+combi_numeric_norm = predict(prep_num, combi_numeric)
+
+combi[,setdiff(num_vars_names, 'Item_Outlet_Sales') := NULL] # removing numeric independent variables
+combi = cbind(combi, combi_numeric_norm)
+
+# splitting the combined dataset 'combi' back to train and test set
+train = combi[1:nrow(train)]
+test = combi[(nrow(train) + 1):nrow(combi)]
+test[,Item_Outlet_Sales := NULL] # removing Item_Outlet_Sales as it contains only NA for test dataset
+
+# Correlated variables
+cor_train = cor(train[,-c('Item_Identifier')])
+corrplot(cor_train, method = 'color', type = 'lower', tl.cex = 0.5, cl.cex = 0.5)
