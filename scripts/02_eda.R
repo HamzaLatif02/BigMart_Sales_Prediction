9564d01f76c4d31177b9f806ea07851f46de0ae5
Hamza Latif <lhamza1020@gmail.com>
2026-02-16 01:30:34 +0000

    added bivariate analysis and respective plots images

--- scripts/02_eda.R
+++ scripts/02_eda.R
@@ -38,3 +38,41 @@ p7 = ggplot(combi %>% group_by(Outlet_Establishment_Year) %>% summarise(Count = 
 p8 = ggplot(combi %>% group_by(Outlet_Type) %>% summarise(Count = n())) + geom_bar(aes(Outlet_Type, Count), stat = 'identity', fill = 'coral1') + geom_label(aes(factor(Outlet_Type), Count, label = Count), vjust = 0.5) + theme(axis.text.x = element_text(size = 8.5))
 
 plot_grid(p7, p8, ncol = 2)
+
+# BIVARIATE ANALYSIS
+
+train = combi[1:nrow(train)] # exctract train data from combined datasets
+
+# Target Variable vs Independent Numerical Variables
+# Item_Weight vs Item_Outlet_Sales
+p9 = ggplot(train) + geom_point(aes(Item_Weight, Item_Outlet_Sales), colour = 'violet', alpha = 0.3) + theme(axis.title = element_text(size = 8.5))
+# Item_Visibility vs Item_Outles_Sales
+p10 = ggplot(train) + geom_point(aes(Item_Visibility, Item_Outlet_Sales), colour = 'violet', alpha = 0.3) + theme(axis.title = element_text(size = 8.5))
+# Item_MRP vs Item_Outlet_Sales
+p11 = ggplot(train) + geom_point(aes(Item_MRP, Item_Outlet_Sales), colour = 'violet', alpha = 0.3) + theme(axis.title = element_text(size = 8.5))
+
+second_row_2 = plot_grid(p10, p11, ncol = 2)
+plot_grid(p9, second_row_2, nrow = 2)
+
+# When the three plots above re run, the following warning message comes up: 
+# Warning message: Removed 1463 rows containing missing values or
+# values outside the scale range (`geom_point()`). 
+
+# Target Variable vs Independent Categorical Variables
+# Item_Type vs Item_Outles_Sales
+p12 = ggplot(train) + geom_violin(aes(Item_Type, Item_Outlet_Sales), fill = 'magenta') + theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text = element_text(size = 6), axis.title = element_text(size = 8.5))
+# Item_Fat_Content vs Item_Outlet_Sales
+p13 = ggplot(train) + geom_violin(aes(Item_Fat_Content, Item_Outlet_Sales), fill = 'magenta') + theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text = element_text(size = 8), axis.title = element_text(size = 8.5))
+# Outlet_Identifier vs Item_Outlet_Sales
+p14 = ggplot(train) + geom_violin(aes(Outlet_Identifier, Item_Outlet_Sales), fill = 'magenta') + theme(axis.text.x = element_text(angle = 45, hjust = 1), axis.text = element_text(size = 8), axis.title = element_text(size = 8.5))
+
+second_row_3 = plot_grid(p13, p14, ncol = 2)
+plot_grid(p12, second_row_3, nrow = 2)
+
+# Check the distribution of the target variable across Outlet_Size
+ggplot(train) + geom_violin(aes(Outlet_Size, Item_Outlet_Sales), fill = 'magenta')
+
+# Examine ramaining variables
+p15 = ggplot(train) + geom_violin(aes(Outlet_Location_Type, Item_Outlet_Sales), fill = 'magenta')
+p16 = ggplot(train) + geom_violin(aes(Outlet_Type, Item_Outlet_Sales), fill = 'magenta')
+plot_grid(p15, p16, nrow =2 )
